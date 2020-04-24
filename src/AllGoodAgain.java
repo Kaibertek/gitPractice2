67025367a05452400c2229e0f301783f3e443b33
Kaibertek <Kaibertek@users.noreply.github.com>
2020-04-24 16:14:34 -0400

    allGoodSecondTry.java

--- src/AllGoodAgain.java
+++
@@ -1,5 +0,0 @@
-public class AllGoodAgain {
-	public static void main(String[] args) {
-		System.out.println("Is this working?");
-	}
-}
---
+++ src/day1/allGoodSecondTry.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class allGoodSecondTry {
+	public static void main(String[] args) {
+		System.out.println("Yo");
+	}
+}
