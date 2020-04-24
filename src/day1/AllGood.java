6fe044e0b9ebe8971dfd8a4453eca2761c829544
Kaisar Anvar <62409400+Kaibertek@users.noreply.github.com>
2020-04-24 16:29:48 -0400

    Update AllGood.java
    here we go.

--- src/day1/AllGood.java
+++ src/day1/AllGood.java
@@ -3,5 +3,9 @@ package day1;
 public class AllGood {
 	public static void main(String[] args) {
 		//I hope everything is GOOD.
+		
+		//this line is added from github website.
+		
+		System.out.print("I added this from Github.");
 	}
 }
